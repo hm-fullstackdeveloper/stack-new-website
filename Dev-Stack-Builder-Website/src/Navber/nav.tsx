c91ee6baab0a80500d9875245775be7b688872b4
Tariqul Islam <hmtarek004@gmail.com>
2026-09-14 17:09:59 +0600

    git commit-5

--- Dev-Stack-Builder-Website/src/Navber/nav.tsx
+++ Dev-Stack-Builder-Website/src/Navber/nav.tsx
@@ -4,14 +4,19 @@ import { Menu } from "lucide-react";
 const nav = () => {
 
     return (
-        <div className="sticky top-0 z-50 
-              flex items-center
+        <div className="sticky top-0 z-50 flex 
+              lg:flex items-center
              justify-between border-2
               border-amber-50 bg-white">
                 <button className="block xl:hidden">
                 <Menu size={28} />
                 </button>
+            
+                <button className="flex items-center justify-center h-5 w-30">
             <img src={Logo} alt="" />
+                </button>
+            
+
             <ul className="hidden lg:flex items-center gap-6">
                 <li className="text-red-500 cursor-pointer">Home</li>
                 <li className="cursor-pointer">Technologis</li>
@@ -19,11 +24,9 @@ const nav = () => {
                 <li className="cursor-pointer">About</li>
                 <li className="cursor-pointer">Contact</li>
             </ul><div className="flex gap-3">
-            <button className="rounded-full border-2 text-black
-           bg-white px-6 py-3 font-semibold border-gray-50 hover-3d
-           hover:bg-gray-50 cursor-pointer">Sign In</button>
-            <button className="rounded-full
-                  bg-pink-500 px-6 py-3 font-semibold
+            <button className=" px-2 py-1 rounded-full  text-black bg-white text-sm font-semibold
+             cursor-pointer">Sign In</button>
+            <button className="px-3 py-1 rounded-full bg-pink-500 text-sm font-semibold
                  text-white hover:bg-pink-400 cursor-pointer">Sign Up</button>
                     
             </div>
